8daf26a970852104ee5b8e16502e02cbe7739d66
Philip Hutchins <flipture@gmail.com>
2014-06-18 08:55:35 -0400

    added setup for archive download and install with single and multicore

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,16 @@
-default[:solr][:version] = "1.4.1+dfsg1-2ubuntu2"
+# For package install
+default[:tomcat][:version] = "6.0.35-1ubuntu3.4"
+
+# For archive download and install
+default[:solr][:version] = ""
+default[:solr][:download_url] = ""
+default[:solr][:archive_name] = ""
+default[:solr][:war_name] = ""
+default[:solr][:base_dir] = "/var/solr"
+default[:solr][:package_version] = "1.4.1+dfsg1-2ubuntu2"
 default[:solr][:max_time] = 5000
+
+default[:solr][:multicore][:enabled] = false
 
 default[:solr][:replication][:enabled] = false
 default[:solr][:replication][:poll_interval] = "00:00:60"
