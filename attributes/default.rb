934291817cf279d66d10355e63e8c7d630763b79
Philip Hutchins <flipture@gmail.com>
2014-06-18 09:37:11 -0400

    updating port num

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,4 +17,4 @@ default[:solr][:multicore][:enabled] = false
 
 default[:solr][:replication][:enabled] = false
 default[:solr][:replication][:poll_interval] = "00:00:60"
-default[:solr][:port] = 8983
+default[:solr][:port] = 8080
