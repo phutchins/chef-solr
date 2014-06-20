ea5391cbec188ed5e8d16d2e35dcf98ada6c76b1
Philip Hutchins <flipture@gmail.com>
2014-06-20 14:58:05 -0400

    setting tomcat version right

--- attributes/tomcat.rb
+++ attributes/tomcat.rb
@@ -3,4 +3,4 @@ default['solr']['tomcat']['xmx'] = "512m"
 default['solr']['tomcat']['xms'] = "256m"
 
 # Overrides for tomcat cookbook
-default['tomcat']['base_version'] = "tomcat6"
+default['tomcat']['base_version'] = "6"
