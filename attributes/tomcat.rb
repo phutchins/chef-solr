f391ea180d6d7dae6e9b84e9f7f679061ba1a4f3
Philip Hutchins <philip.hutchins@dealermatch.com>
2014-07-15 12:30:33 -0400

    adding protection from going over total memory on a host with xmx and xms settings'

--- attributes/tomcat.rb
+++ attributes/tomcat.rb
@@ -2,6 +2,8 @@
 default['tomcat']['java_options_custom'] = ""
 default['solr']['tomcat']['xmx'] = "512m"
 default['solr']['tomcat']['xms'] = "256m"
+default['solr']['tomcat']['xmx_percent'] = 70
+default['solr']['tomcat']['xms_percent'] = 40
 
 # Overrides for tomcat cookbook
 default['tomcat']['base_version'] = "6"
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,17 @@
+['xmx_percent', 'xms_percent'].each do |attr|
+  if node['solr']['tomcat'][attr].to_i >= 90 then
+    log "xmx_issue" do
+      message "#{attr} value #{node['solr']['tomcat'][attr]}% is too high! Overriding with a sane value"
+      level :warn
+    end
+    node.normal['solr']['tomcat'][attr] = 80
+  end
+end
+
+['xmx', 'xms'].each do |attr|
+  node.normal['solr']['tomcat'][attr] = node['memory'] * node['solr']['tomcat']["#{attr}_percent"].to_i/100
+end
+
 custom_options = node[:tomcat][:java_options_custom]
 node.normal[:tomcat][:java_options] = "-Xmx#{node['solr']['tomcat']['xmx']} -Xms#{node['solr']['tomcat']['xms']} -Djava.awt.headless=true -XX:+UseConcMarkSweepGC #{custom_options}"
 
