520d2b27d405a681f5636127d72fb1a2fe566fe4
Philip Hutchins <philip.hutchins@dealermatch.com>
2014-07-23 15:18:58 -0400

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ description		"Install and configure Apache Solr"
 maintainer		"Philip Hutchins"
 maintainer_email 	"flipture@gmail.com"
 long_description 	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          	"0.0.4"
+version          	"0.0.5"
 
 depends 'tomcat'
